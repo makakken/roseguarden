a56316027a94273cab84dfc94536967eca1a7489
mdrobisch <m.drobisch@googlemail.com>
2021-07-24 22:02:54 +0200

    STORE send mail in INBOX.Sent

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -5,7 +5,7 @@ from testEnv import create_testEnv
 
 
 @pytest.fixture(scope="function")
-def set_up_and_tear_down_database():
+def base_setup():
     """
     This function prepares and destruct the flask application and the database
     before and after each test.
