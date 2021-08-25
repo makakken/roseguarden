c3db5bd53e6d81373102ccc3a46654ec2115ff27
mdrobisch <m.drobisch@googlemail.com>
2021-08-25 09:58:05 +0200

    REARANGE tests of usermanager

---
+++ backend/tests/test_usermanager/test_base_functionality.py
@@ -0,0 +1,20 @@
+import time
+from core.users import userManager
+from core.users.enum import UserAuthenticatorStatus
+from core.workspaces import workspaceManager
+from core.workspaces.workspaceHooks import WorkspaceHooks
+
+import pytest
+
+from core.users.models import User
+
+
+def test_public_authenticator_gets_set_to_default(base_setup):
+    """Test that an empty authenticator will set to default automatically
+
+    Uses the "roseguarden@fabba.space" user from the devEnv.py base environemnt.
+    """
+    u = userManager.getUserByAuthenticator("111.222.333.000", "")
+    assert u is not None
+    assert u.authenticator_public_key != ""
+
